a5d3346aef87c3755484535a41688d5c65dc4334
oxy18 <84609520+0x18e@users.noreply.github.com>
2025-06-29 06:01:12 -0400

    feat: add resource handling

--- src/ResourceHandler.cpp
+++ src/ResourceHandler.cpp
@@ -1,1 +1,50 @@
 #include "ResourceHandler.h"
+
+ResourceHandler ResourceHandler::m_Instance;
+
+void ResourceHandler::Precache() {
+	// load all resources here.
+	this->m_Models["obj_debug"] = std::make_unique<Model>("assets/models/kiwi/kiwi.obj");
+
+
+	this->m_Shaders["base_shader"] = std::make_unique<Shader>("assets/shaders/model_loading.vs", "assets/shaders/model_loading.fs");
+}
+
+void ResourceHandler::ClearModel(const std::string& name) {
+	// Used when changing levels to remove unneeded models. Small optimizations
+	this->m_Models.erase(name);
+}
+
+void ResourceHandler::ClearShader(const std::string& name) {
+	this->m_Shaders.erase(name);
+}
+
+Model* ResourceHandler::GetModel(const std::string& name) {
+	for (auto& pair : this->m_Models) {
+		if (pair.first == name) {
+			return pair.second.get(); // get returns non ownable pointer, the raw pointer
+		}
+	}
+	LOG("Missing model: " << name);
+	return nullptr;
+}
+
+Shader* ResourceHandler::GetShader(const std::string& name){
+	
+	for (auto& pair : this->m_Shaders) {
+		if (pair.first == name) {
+
+			return pair.second.get();
+		}
+	}
+	LOG("Missing shader: " << name);
+	return nullptr;
+}
+
+ResourceHandler::~ResourceHandler() {
+	// RAII from unique_ptr resources
+}
+
+ResourceHandler::ResourceHandler() {
+
+}
--- src/ResourceHandler.h
+++ src/ResourceHandler.h
@@ -1,3 +1,29 @@
 #pragma once
 #include<iostream>
+#include<unordered_map>
+#include "Model.h"
 
+
+// Cache resources, call wherever, singleton.
+class ResourceHandler {
+public:
+	static ResourceHandler& Get() {
+		return m_Instance;
+	}
+	void Precache();
+	void ClearModel(const std::string& name);
+	void ClearShader(const std::string& name);
+
+	Model* GetModel(const std::string& name);
+	Shader* GetShader(const std::string& name);
+	~ResourceHandler();
+	ResourceHandler();
+protected:
+private:
+	 
+	static ResourceHandler m_Instance;
+
+	// all resource containers
+	std::unordered_map<std::string, std::unique_ptr<Model>> m_Models;
+	std::unordered_map<std::string, std::unique_ptr<Shader>> m_Shaders;
+};
